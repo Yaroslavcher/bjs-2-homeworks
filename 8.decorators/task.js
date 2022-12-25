dab07b5fbd5dc72d9c490a3629fee1b9ad2ab1ac
unknown <yaroslavcher@gmail.com>
2022-12-25 18:04:23 +0300

    1st commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,7 +1,20 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = {};
+    return (...args) => {
+        const hash = args.join(',');
+        if(hash in cache){
+            return cache[hash];
+        }
+        const result = func(...args);
+        cache[hash] = result;
+        return result;
+        //return cache[hash] = func(...args);  
+    }  
 }
+
+const addAndMultiply = (a, b, c) => (a + c) * c;
+const cachedAddAndMultiply = cachingDecoratorNew(addAndMultiply);
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
