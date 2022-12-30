7cac78335ca128266f0f8a83deb76ce379c506f9
unknown <yaroslavcher@gmail.com>
2022-12-30 22:31:03 +0300

    где должно быть условие для 1го вызова

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,16 +1,24 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-    let cache = {};
-    return (...args) => {
-        const hash = args.join(',');
-        if(hash in cache){
-            return cache[hash];
-        }
-        const result = func(...args);
-        cache[hash] = result;
-        return result;
-        //return cache[hash] = func(...args);  
-    }  
+  const cache = [];
+  return function(...args) {
+    const hash = md5(args); //получаю хэш по заданным аргументам
+    const objectInCache = cache.find(
+      objectInCache => objectInCache.hash === hash); //ищу заданный хэш(-и)
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.value); //если хэш найден (аргументы уже вычислены),
+      return "Из кэша: " + objectInCache.value; //то возвращаю значение из кэша
+    }
+
+    const value = func(...args);
+    cache.push({hash, value});
+    const maxCache = 5;
+      if(cache.length > maxCache){
+      cache.shift();
+      }
+    console.log("Вычисляем: " + value);
+    return "Вычисляем: " + value;
+  }
 }
 
 const addAndMultiply = (a, b, c) => (a + c) * c;
@@ -18,5 +26,35 @@ const cachedAddAndMultiply = cachingDecoratorNew(addAndMultiply);
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+  return function(...args) {
+    if(timeoutId){
+      console.log("удалил текущий таймаут");
+      clearTimeout(timeoutId);
+      } else {
+        let delay = 0;
+  }
+    console.log("создаю новый таймаут");
+    timeoutId = setTimeout(() => {
+      timeoutId = null;
+      console.log(func(...args));
+      console.log("вызвал колбэк");
+    }, delay);
+  }
 }
+
+const sendSignal = (signalOrder, delay) => console.log("Сигнал отправлен", signalOrder, delay);
+const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
+setTimeout(() => upgradedSendSignal(1, 0)); // Сигнал отправлен + будет запланирован асинхронный запуск, который будет проигнорирован, так как следующий сигнал отменит предыдущий (300 - 0 < 2000)
+setTimeout(() => upgradedSendSignal(2, 300), 300); // проигнорировано, так как следующий сигнал отменит предыдущий (900 - 300 < 2000)
+setTimeout(() => upgradedSendSignal(3, 900), 900); // проигнорировано, так как следующий сигнал отменит предыдущий (1200 - 900 < 2000)
+setTimeout(() => upgradedSendSignal(4, 1200), 1200); // проигнорировано, так как следующий сигнал отменит предыдущий (2300 - 1200 < 2000)
+setTimeout(() => upgradedSendSignal(5, 2300), 2300); // Сигнал отправлен, так как следующий вызов не успеет отменить текущий: 4400-2300=2100 (2100 > 2000)
+setTimeout(() => upgradedSendSignal(6, 4400), 4400); // проигнорировано, так как следующий сигнал отменит предыдущий (4500 - 4400 < 2000)
+setTimeout(() => upgradedSendSignal(7, 4500), 4500); // Сигнал будет отправлен, так как последний вызов debounce декоратора (спустя 4500 + 2000 = 6500) 6,5с
+
+// let timeoutId = null;
+// if (timeoutId) {
+//   debounceDecoratorNew();
+// }
+// func(...args);
